2ff3a0ae942abb054204f9af4a70726ea6b67ef4
Alexey <chiglincev2010@bk.ru>
2023-12-26 15:09:18 +0500

    Update content capitalize.test.js

--- test/test-capitaize.js
+++ test/test-capitaize.js
@@ -1,5 +1,6 @@
 import capitalize from "../src/capitalize.js";
-import { strict as assert } from 'node:assert';
-assert.equal(capitalize(''), 'l');
-assert.equal(capitalize('hello'), 'Hello');
+import { strict as assert} from 'node:assert';
+assert.deepStrictEqual(capitalize(''), '');
+assert.deepStrictEqual(capitalize('hello'), 'Hello');
+assert.deepStrictEqual(capitalize('world'), 'World');
 console.log('expected');
